fb8f2b9de5d5b8ee428ee1c692df36fac03f8a38
Matthew Fung <matthew_fung@hotmail.com>
2021-12-25 18:28:40 -0500

    created theme

--- inventory-x/pages/_app.js
+++ inventory-x/pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+
+export const themeOptions = createTheme({
+  palette: {
+    primary: {
+      main: "#301E5D",
+    },
+    secondary: {
+      main: "#8A84E2",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+    },
+    h2: {
+      fontSize: "1.8rem",
+    },
+    h3: {
+      fontSize: "1.5rem",
+    },
+    h4: {
+      fontSize: "1.1rem",
+    },
+    h5: {
+      fontSize: "0.7rem",
+    },
+    h6: {
+      fontSize: "0.7rem",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={themeOptions}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
